1ff6c7d857eabd4ccaa79de23bad59493e5878ff
Majid-8199 <majidkhobar@gmail.com>
2025-06-07 16:15:55 +0300

    Phase 2

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,11 +1,27 @@
 import HeroSection from './components/HeroSection';
-import AllBlogs from './components/AllBlogs'
+import AllBlogs from './components/AllBlogs';
+import { Suspense } from 'react';
+import Image from 'next/image';
 
 const Blog = () => {
   return (
     <>
       <HeroSection />
-      <AllBlogs />
+      <Suspense
+        fallback={
+          <div className="py-20 text-center">
+            <Image
+              src="/services/amer.webp"
+              alt="Loading..."
+              width={50}
+              height={50}
+              className="animate-spin inline-block"
+            />
+          </div>
+        }
+      >
+        <AllBlogs />
+      </Suspense>
     </>
   );
 };
